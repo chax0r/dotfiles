ef89dbcb3821477ca4bb567dadd931433b1d2b55
Chinmay Kale <chinmay.kale@quantumworkplace.com>
2025-07-23 12:51:11 -0400

    fixed a typo

--- nvim/lua/chax0r/lsp-config.lua
+++ nvim/lua/chax0r/lsp-config.lua
@@ -2,9 +2,11 @@ vim.lsp.enable('lua_ls')
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if not client then
+			return
+		end	
 		if client:supports_method('textDocument/completion') then
 			vim.opt.completeopt = {'menu', 'menuone', 'noinsert', 'fuzzy', 'popup'}
-			vim.lsp.
 			vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
 			vim.keymap.set('i', '<C-Space>', function()
 				vim.lsp.completion.get()
